8d55ca0b8bd3e7580eb2d861cd02cc03a83119c7
onurdogustemel <onurdogustemel@yahoo.com>
2021-09-16 17:57:19 +0300

    modified

---
+++ odevler/odev19.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class odev19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n;
+		
+		System.out.println("Enter a number: ");
+		n = input.nextInt();
+		
+		for(int i = 1,j=1; i <= n; i*=4) {
+			System.out.println("Power of 4: " + i);
+		}
+		
+		System.out.println("");
+		
+		for(int j = 1; j <= n; j*=5) {
+			System.out.println("Power of 5: " + j);
+		}
+
+	}
+
+}
