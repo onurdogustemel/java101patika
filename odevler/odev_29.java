ae312154e81598586524e76bca65f73156751216
onurdogustemel <onurdogustemel@yahoo.com>
2021-09-19 16:53:56 +0300

    modified

---
+++ odevler/odev_29.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class odev_29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int number;
+		
+		System.out.println("Enter a number: ");
+		number = input.nextInt();
+		
+		for (int i = number; i >= 0 ; i--) {
+            for (int j = (number - i); j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+	}
+
+}
