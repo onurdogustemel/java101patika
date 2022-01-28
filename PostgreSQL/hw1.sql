869f768c647f890227f36a0ba39d0c0c007555e5
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-28 23:28:44 +0300

    PostgreSQL Homework 1

---
+++ PostgreSQL/hw1.sql
@@ -0,0 +1,8 @@
+-- 1. Command
+-- SELECT title, description FROM film WHERE length > 60 AND length < 75;
+-- 2. Command
+-- SELECT*FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+-- 3. Command
+--SELECT*FROM customer WHERE first_name = 'Mary';
+-- 4. Command
+--SELECT*FROM film WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
