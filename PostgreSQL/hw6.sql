a37c308a954bc97a4d2d0783adbff758cf9bb1a8
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-30 00:57:39 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw6.sql
@@ -0,0 +1,8 @@
+-- 1.Command
+-- SELECT ROUND(AVG(rental_rate),3) FROM film;
+-- 2. Command
+-- SELECT COUNT(title) FROM film WHERE title LIKE 'C%';
+-- 3. Command
+-- SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
+-- 4. Command
+-- SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150;
