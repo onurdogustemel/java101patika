385005a499b3ced695513637123e8885fa4c368c
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-29 16:38:27 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw2.sql
@@ -0,0 +1,6 @@
+-- 1. Command
+-- SELECT title, replacement_cost FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+-- 2. Command
+-- SELECT first_name, last_name FROM actor WHERE first_name IN('Penelope','Nick','Ed');
+-- 3. Command
+-- SELECT*FROM film WHERE (rental_rate IN(0.99, 2.99, 4.99)) AND (replacement_cost IN(12.99, 15.99, 28.99));
