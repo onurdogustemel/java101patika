28ecbdaf1459ea8bf30157d0a646f2efece9ccf0
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-29 23:16:42 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw5.sql
@@ -0,0 +1,6 @@
+-- 1. Command
+-- SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
+-- 2. Command
+-- SELECT * FROM film WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5;
+-- 3. Command
+--SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
