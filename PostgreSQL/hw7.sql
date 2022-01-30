56488e98d48e31dc2895b7ddcb37ccd46ee39f2b
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-30 18:15:46 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw7.sql
@@ -0,0 +1,8 @@
+-- 1. Command
+-- SELECT rating, COUNT(*) FROM film GROUP BY rating;
+-- 2. Command
+-- SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+-- 3. Command
+--SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+-- 4. Command
+--SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
