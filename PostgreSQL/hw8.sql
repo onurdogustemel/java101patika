2e195d849164bb8b00b29aa9841ada81c8584319
onurdogustemel <onurdogustemel@yahoo.com>
2022-01-31 15:52:39 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw8.sql
@@ -0,0 +1,24 @@
+-- 1. Command
+-- CREATE TABLE employee(
+--	 id INTEGER PRIMARY KEY,
+--	 name VARCHAR(50) NOT NULL,
+--	 birthday DATE NOT NULL,
+--	 email VARCHAR(100)
+-- );
+
+-- 2. command
+-- UPDATE employee
+-- SET name = 'Jada Smith',
+--	 birthday = '1967-04-05',
+--	 email = 'jadasmith@alibaba.com'
+-- WHERE id = 1;
+
+-- UPDATE employee
+-- SET name = 'Will Smith',
+--	 birthday = '1966-09-08',
+--	 email = 'willsmith@alibaba.com'
+-- WHERE id BETWEEN 2 AND 5;
+
+-- 3. Command
+
+-- DELETE FROM employee WHERE id BETWEEN 1 AND 5;
