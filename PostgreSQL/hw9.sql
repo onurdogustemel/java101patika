ca28d2be1645ce8bbba6ebc88134101e5c4b6d33
onurdogustemel <onurdogustemel@yahoo.com>
2022-02-01 19:19:14 +0300

    PostgreSQL Homeworks

---
+++ PostgreSQL/hw9.sql
@@ -0,0 +1,8 @@
+-- 1. Command
+-- SELECT * FROM city INNER JOIN country ON city.country_id = country.country_id;
+
+-- 2. Command
+--SELECT payment.payment_id, first_name, last_name FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- 3. Command
+--SELECT rental.rental_id ,first_name, last_name FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id;
